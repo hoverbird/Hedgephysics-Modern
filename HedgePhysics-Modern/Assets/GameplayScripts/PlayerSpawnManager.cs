82d00436d8119ece9720bbbf24a2d3ad2d812820
Patrick Ewing <patrick@chanceagency.com>
2019-07-16 15:41:13 -0700

    Upgrade everything for Unity 2018.4.3

--- HedgePhysics-Modern/Assets/GameplayScripts/PlayerSpawnManager.cs
+++ HedgePhysics-Modern/Assets/GameplayScripts/PlayerSpawnManager.cs
@@ -2,10 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
-using UnityEngine.PostProcessing;
+using UnityEngine.Rendering.PostProcessing;
 
 public class PlayerSpawnManager : NetworkBehaviour {
-    public PostProcessingProfile PostProcessingProfile;
+    public PostProcessProfile PostProcessingProfile;
 	// Use this for initialization
 	void Start () {
         StartCoroutine(UpdatePosition());
@@ -33,8 +33,8 @@ public class PlayerSpawnManager : NetworkBehaviour {
                 FindObjectOfType<PlayerBhysics>().transform.position = transform.position;
             }
         }
-        if (FindObjectOfType<PostProcessingBehaviour>()) {
-            FindObjectOfType<PostProcessingBehaviour>().profile = PostProcessingProfile;
+        if (FindObjectOfType<PostProcessVolume>()) {
+            FindObjectOfType<PostProcessVolume>().profile = PostProcessingProfile;
         }
     }
 }
